803b688b367b79fe9b1be5d2f2e477de43bbb9ad
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-09-14 22:33:42 -0400

    Transition appliqué dans un setInterval distinct

--- js/style.js
+++ js/style.js
@@ -58,6 +58,7 @@ choisirDesImagesAuHasard();
 afficherUneImage();
 
 setInterval(afficherUneImage, 3000);
+setInterval(appliquerTransition, 3000);
 
 
 /**
@@ -120,15 +121,18 @@ function afficherUneImage() {
   //on augmente le numéro de l'index
   noIndex++;
 
-  //on applique la classe .carrousel_img--activer
-//on applique la classe .carrousel_img--activer
-
-carrouselImagesClasse.classList.add("carrousel_img-activer")
-  
-
   //On réinitialise le compteur s'il atteint la fin de l'index
   if (noIndex == carrouselImages.length) {
     noIndex = 0;
   }
-
 }
+
+function appliquerTransition() {
+  //on applique la classe .carrousel_img--activer
+  carrouselImagesClasse.classList.add("carrousel_img-activer");
+}
+
+
+
+
+
